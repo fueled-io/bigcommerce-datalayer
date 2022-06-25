3b364eafae623640b746a18ba93c4db0cf634bb3
Eddie Svirsky <1399489+esvirsky@users.noreply.github.com>
2022-06-24 18:02:27 -0600

    updated with more info about customer, cart items, and search

--- src/index.js
+++ src/index.js
@@ -23,34 +23,50 @@
   }
 
   function getShopper() {
-    let name = '{{customer.name}}';
-    name = name.split(" ");
     return clean({
       customer_id: '{{customer.id}}',
       email: '{{customer.email}}',
-      first_name: name[0],
-      last_name: name[1],
+      name: '{{customer.name}}',
+      phone: '{{customer.phone}}',
+//    {{#if customer.shipping_address}}
+          shipping_address: {
+            address1: '{{customer.shipping_address.address1}}',
+            address2: '{{customer.shipping_address.address2}}',
+            city: '{{customer.shipping_address.city}}',
+            company: '{{customer.shipping_address.company}}',
+            country: '{{customer.shipping_address.country}}',
+            first_name: '{{customer.shipping_address.first_name}}',
+            last_name: '{{customer.shipping_address.last_name}}',
+            address_id: '{{customer.shipping_address.id}}',
+            phone: '{{customer.shipping_address.phone}}',
+            state: '{{customer.shipping_address.state}}',
+            zip: '{{customer.shipping_address.zip}}',
+          }
+//    {{/if}}
     });
   }
 
   function getItems() {
     var items = [];
 //    {{#each this}}
-//      {{#or (if @key '===' "products") (if @key '===' "category")}}
+//      {{#or (if @key '===' "products") (if @key '===' "category") (if @key '===' "cart")}}
 //        {{#each this}}
-//          {{#or (if @key '===' "new") (if @key '===' "featured") (if @key '===' "products") (if @key '===' "category")}}
+//          {{#or (if @key '===' "new") (if @key '===' "featured") (if @key '===' "products") (if @key '===' "category") (if @key '===' "items")}}
 //            {{#each this}}
                 var item = clean({
                   item_name: htmlDecode('{{name}}'),
-                  item_id: '{{id}}',
+                  item_id: '{{product_id}}' ? '{{product_id}}' : '{{id}}',
                   sku: '{{sku}}',
-                  price: parseFloat('{{price.without_tax.value}}'),
+                  price: "{{price.value}}" ? parseFloat('{{price.value}}') : parseFloat('{{price.without_tax.value}}'),
                   item_brand: htmlDecode('{{brand.name}}'),
                   item_url: htmlDecode('{{url}}'),
                   item_image_url: htmlDecode('{{image.data}}'),
                   item_list_name: htmlDecode('{{../category.name}}'),
-                  item_list_id: '{{../category.id}}'
+                  item_list_id: '{{../category.id}}',
+                  quantity: '{{quantity}}' ? parseInt('{{quantity}}') : undefined
                 });
+
+
 
                 //{{#each category}}
                 var index = parseInt('{{@index}}');
@@ -83,7 +99,7 @@
     return item;
   }
 
-  function pushDataLayer(event, items) {
+  function pushDataLayerEcommerce(event, items) {
     dataLayer.push({
         event: event,
         ecommerce: {
@@ -92,23 +108,27 @@
     });
   }
 
-  if (pageType == 'category') {
-    pushDataLayer('view_item_list', getItems());
+  if (pageType === 'category') {
+    pushDataLayerEcommerce('view_item_list', getItems());
   }
   else if (pageType === 'product') {
-    pushDataLayer('view_item', [getItem()]);
-    }
+    pushDataLayerEcommerce('view_item', [getItem()]);
+    } else if (pageType === 'search') {
+        dataLayer.push({
+        event: 'search',
+        search_term: '{{forms.search.query}}'
+    });
+  }
 
   window.eecGetShopper = getShopper;
   window.eecGetItems = getItems;
   window.eecGetItem = getItem;
-  window.eecPushDataLayer = pushDataLayer;
   window.eecHtmlDecode = htmlDecode;
   window.eecClean = clean;
 
   dataLayer['eec'] = {};
 
-  //{{#or (if category.products) (if products.new) (if products.featured) (if products.top_sellers)}}
+  //{{#or (if category.products) (if products.new) (if products.featured) (if products.top_sellers) (if cart.items)}}
   dataLayer['eec']['items'] = getItems();
   //{{/or}}
 
@@ -131,5 +151,10 @@
   //{{#if category.name}}
   dataLayer['eec']['item_list_name'] = htmlDecode('{{category.name}}');
   //{{/if}}
+
+  //{{#if forms.search.query}}
+  dataLayer['eec']['search_query'] = htmlDecode('{{forms.search.query}}');
+  //{{/if}}
+
 })
 ();
